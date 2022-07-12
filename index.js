62f0774e7b9c324aa966af1e63879e9b2b9b4380
Hunter Padgett <gizmokabanjo@gmail.com>
2022-07-12 00:05:26 -0400

    added questions array

--- index.js
+++ index.js
@@ -1,8 +1,77 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
 // TODO: Include packages needed for this application
 // console.log("ayayya")
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Give a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information of your project?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines of your project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the test instructions of your project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'choose a license for your application:',
+      choices: ['MIT', 'Apache', 'GPL', 'BSD'],
+      default: 'MIT'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+    
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -12,3 +81,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+questions();
